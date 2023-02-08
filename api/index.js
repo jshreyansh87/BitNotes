024dbe1fd21de9eda3f716ab360a21b4071b1bc6
Shreyansh Jain <jshreyansh87@gmail.com>
2023-02-09 00:38:58 +0530

    Add models and routes. Create a test post request with users

--- api/index.js
+++ api/index.js
@@ -5,9 +5,14 @@ ConnectToDatabase();
 const app = express()
 const port = 3000
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.use('/api/auth', require('./Routes/Auth'));
+app.use('/api/notes', require('./Routes/Notes'));
 
 app.listen(port, () => {
   console.log(`BitNotes listening at http://localhost:${port}`)
