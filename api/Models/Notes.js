d1e3cf889bcc1b1e714a52aaa275bd8b0fae5ffa
Shreyansh Jain <jshreyansh87@gmail.com>
2023-02-11 00:44:21 +0530

    Add Update notes api

--- api/Models/Notes.js
+++ api/Models/Notes.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const NotesSchema = new Schema({
-    notes: {
+    user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'users'
     },
--- api/Routes/Notes.js
+++ api/Routes/Notes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const FetchUser = require('../middlewares/FetchUser');
+const FetchUser = require('../Middlewares/FetchUser');
 const Note = require('../Models/Notes');
 const { body, validationResult } = require('express-validator');
 
-router.get('/fetch-my-notes', FetchUser, async (req, res) => {
+router.get('/', FetchUser, async (req, res) => {
     try {
         const notes = await Note.find({ user: req.user.id });
         return res.json({ notes });
@@ -13,7 +13,7 @@ router.get('/fetch-my-notes', FetchUser, async (req, res) => {
     }
 });
 
-router.post('/create', FetchUser, [
+router.post('/', FetchUser, [
     body('title', 'Enter a valid title').isLength({ min: 3 }),
     body('description', 'Enter a valid description').isLength({ min: 5 })
 ], async (req, res) => {
@@ -29,6 +29,35 @@ router.post('/create', FetchUser, [
             description: req.body.description,
             tag: req.body.tag ?? Note.tag
         });
+
+        return res.json({ note });
+    } catch (error) {
+        return res.status(500).json({ error: "An error occured while processing your request" });
+    }
+});
+
+router.put('/:noteId', FetchUser, [
+    body('title', 'Enter a valid title').isLength({ min: 3 }),
+    body('description', 'Enter a valid description').isLength({ min: 5 })
+], async (req, res) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        let note = await Note.findOne({ _id: req.params.noteId, user: req.user.id });
+        if (!note) {
+            return res.status(404).json({ error: "Note not found" });
+        }
+
+        note = await Note.findByIdAndUpdate(req.params.noteId, {
+            $set: {
+                title: req.body.title ?? note.title,
+                description: req.body.description ?? note.description,
+                tag: req.body.tag ?? note.tag
+            }
+        }, { new: true });
 
         return res.json({ note });
     } catch (error) {
