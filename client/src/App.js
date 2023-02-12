3228ae080a3ffde25cea62532074e1e5b870152b
Shreyansh Jain <jshreyansh87@gmail.com>
2023-02-12 14:58:19 +0530

    Add home, about and error page with react routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Error from './components/Error';
+
 
 function App() {
   return (
     <div className="App">
-      BitNotes
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/components/Error.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div>
+      <h1>Oops!</h1>
+      <p>Sorry, Page Not Found</p>
+    </div>
+  )
+}
+
+export default Error
