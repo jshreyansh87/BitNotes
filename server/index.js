a8da1ab046185cf5307b6253a0fee7f3d2dde802
Shreyansh Jain <jshreyansh87@gmail.com>
2023-02-14 00:16:15 +0530

    Add cors in server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const ConnectToDatabase = require('./DatabaseContext');
+const cors = require('cors');
 
 ConnectToDatabase();
-const app = express()
-const port = 5000
+const app = express();
+const port = 5000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
